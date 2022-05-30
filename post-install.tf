d32f2c5b6da7c972a4cdb9557ce1eda157c98905
Nicolas MASSE <nicolas.masse@itix.fr>
2022-05-30 17:34:53 +0200

    add kubepray$

--- post-install.tf
+++ post-install.tf
@@ -1,9 +1,3 @@
-resource "local_file" "registry_pv" {
-  content         = templatefile("${path.module}/templates/registry-pv.yaml", { nfs_server = local.storage_node.ip })
-  filename        = ".clusters/${var.cluster_name}/registry-pv.yaml"
-  file_permission = "0644"
-}
-
 resource "local_file" "nfs_provisioner" {
   content         = templatefile("${path.module}/templates/nfs-provisioner.yaml", { nfs_server = local.storage_node.ip })
   filename        = ".clusters/${var.cluster_name}/nfs-provisioner.yaml"
@@ -11,7 +5,7 @@ resource "local_file" "nfs_provisioner" {
 }
 
 resource "local_file" "ansible_inventory" {
-  content         = templatefile("${path.module}/templates/inventory", { nodes = local.all_nodes })
-  filename        = ".clusters/${var.cluster_name}/inventory"
+  content         = templatefile("${path.module}/templates/inventory.yaml", { nodes = local.all_nodes, lb_ip = local.lb_node.ip })
+  filename        = ".clusters/${var.cluster_name}/inventory.yaml"
   file_permission = "0644"
 }
